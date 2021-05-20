9569e9a302a761e4bc76715cb3cf495163a7f835
danielaparker <danielaparker@yahoo.com>
2021-05-20 15:08:45 -0400

    Initial commit

--- src/JsonPath/JsonPath.cs
+++ src/JsonPath/JsonPath.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Jsoncons.JsonPathLib
 {
-    public class JsonPath
+    public class Class1
     {
     }
 }
--- tests/JsonPath.Tests/JsonPathTests.cs
+++ tests/JsonPath.Tests/JsonPathTests.cs
@@ -4,12 +4,21 @@ using NUnit.Framework;
 
 namespace Jsoncons.JsonPathLib.Tests
 {
+    [TestFixture]
     public class JsonPathTests
     {
-        public static void Test(string j)
+        [Test]
+        public void Test()
         {
             string path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"test_data\identifiers.json");
             Console.Write(path);
+        }
+        [TestCase(12, 3, 4)]
+        [TestCase(12, 2, 6)]
+        [TestCase(12, 4, 3)]
+        public void DivideTest(int n, int d, int q)
+        {
+            Assert.AreEqual(q, n / d);
         }
     }
 }
