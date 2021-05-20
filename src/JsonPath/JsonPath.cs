2080ecb73138d97661df54e33a1f54b7d11d511f
danielaparker <danielaparker@yahoo.com>
2021-05-20 11:06:19 -0400

    Initial commit

---
+++ src/JsonPath/JsonPath.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Jsoncons.JsonPathLib
+{
+    public class JsonPath
+    {
+    }
+}
---
+++ tests/JsonPath.Tests/JsonPath.Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Jsoncons.JsonPathLib.Tests
+{
+    public class JsonPathTests
+    {
+        public static void Test(string j)
+        {
+            string path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"test_data\identifiers.json");
+            Console.Write(path);
+        }
+    }
+}
