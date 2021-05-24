3aceb1aae014b4cd7eb7e03c212fa966a7203b8e
danielaparker <danielaparker@yahoo.com>
2021-05-24 12:16:30 -0400

    Initial commit

---
+++ src/JsonPath/JsonPathExpression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonCons.JsonPathLib
+{
+    public class JsonPathExpression
+    {
+        public static JsonPathExpression CreateInstance(string expr)
+        {
+            return new JsonPathExpression();
+        }
+    }
+}
--- tests/JsonPath.Tests/JsonPathTests.cs
+++ tests/JsonPath.Tests/JsonPathTests.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Diagnostics;
 using System.Text.Json;
+using JsonCons.JsonPathLib;
 using NUnit.Framework;
 
 namespace JsonCons.JsonPathLib.Tests
@@ -9,17 +10,36 @@ namespace JsonCons.JsonPathLib.Tests
     [TestFixture]
     public class JsonPathTests
     {
-        public void RunJsonTests(string path)
+        public void RunJsonPathTests(string path)
         {
             string text = System.IO.File.ReadAllText(path);
-            JsonDocument tests = JsonDocument.Parse(text);
+            JsonDocument doc = JsonDocument.Parse(text);
+            var testsEnumeratable = doc.RootElement.EnumerateArray();
+
+            foreach (var testGroup in testsEnumeratable)
+            {
+                var given = testGroup.GetProperty("given");
+                var testCases = testGroup.GetProperty("cases");
+                var testCasesEnumeratable = testCases.EnumerateArray();
+                foreach (var testCase in testCasesEnumeratable)
+                {
+                    var exprElement = testCase.GetProperty("expression");
+                    var expression = JsonPathExpression.CreateInstance(exprElement.ToString());
+                    JsonElement expected;
+                    if (testCase.TryGetProperty("result", out expected))
+                    {
+                        TestContext.WriteLine(expected.ToString());
+                    }
+                }
+            }
         }
 
         [Test]
         public void Test()
         {
+            //TestContext.WriteLine("Message...");
             var path = System.IO.Path.Combine(TestContext.CurrentContext.WorkDirectory, @"..\..\test_data\identifiers.json");
-            RunJsonTests(path);
+            RunJsonPathTests(path);
         }
     }
 }
