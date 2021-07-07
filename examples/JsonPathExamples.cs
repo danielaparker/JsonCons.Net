b657b1c310a8e89e51edb6e2f2f3cc4e43bb96f4
danielaparker <danielaparker@yahoo.com>
2021-07-07 10:50:45 -0400

    Added examples

---
+++ examples/JsonPathExamples.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Text.Json;
+using JsonCons.JsonPathLib;
+
+namespace JsonCons.Examples
+{
+}
---
+++ examples/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonCons.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
