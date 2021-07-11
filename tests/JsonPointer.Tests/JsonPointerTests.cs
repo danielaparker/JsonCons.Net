7fd8cfe79cdc59c9c57453e1bdd2c5e1ce35894b
danielaparker <danielaparker@yahoo.com>
2021-07-11 08:37:26 -0400

    Add JsonPointer.Tests

---
+++ tests/JsonPointer.Tests/JsonPointerTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JsonCons.JsonPointerLib;
+
+namespace JsonPointer.Tests
+{
+    [TestClass]
+    public class JsonPointerTests
+    {
+        [TestMethod]
+        public void TestJsonPointer()
+        {
+        }
+    }
+}
