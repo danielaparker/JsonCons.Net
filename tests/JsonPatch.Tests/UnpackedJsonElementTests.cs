390e194dd87d73ecc60be51f0930754c1c61e0e2
danielaparker <danielaparker@yahoo.com>
2021-07-12 21:07:53 -0400

    JsonPatchLib.Tests

--- tests/JsonPatch.Tests/UnpackedJsonElementTests.cs
+++ tests/JsonPatch.Tests/UnpackedJsonElementTests.cs
@@ -120,5 +120,51 @@ namespace JsonPatchLib.Tests
             JsonElementEqualityComparer.Instance.Equals(expected.RootElement,
                                                         result.RootElement);
         }
+
+        [TestMethod]
+        public void TestRemoveArrayElement()
+        {
+            using var doc = JsonDocument.Parse(@"{ ""foo"": [ ""bar"", ""qux"", ""baz"" ] }");
+
+            using var expected = JsonDocument.Parse(@"{ ""foo"": [ ""bar"", ""baz"" ] }");
+
+            var unpacked = UnpackedJsonElement.Unpack(doc.RootElement);
+
+            var location = JsonPointer.Parse(@"/foo/1");
+
+            Assert.IsTrue(location.TryRemove(ref unpacked));
+            JsonDocument result = unpacked.ToJsonDocument();
+
+            JsonElementEqualityComparer.Instance.Equals(expected.RootElement,
+                                                        result.RootElement);
+        }
+
+        [TestMethod]
+        public void TestReplaceObjectValue()
+        {
+            using var doc = JsonDocument.Parse(@"{
+              ""baz"": ""qux"",
+              ""foo"": ""bar""
+            }");
+            using var value = JsonDocument.Parse(@"""boo""");
+
+            using var expected = JsonDocument.Parse(@"
+            {
+              ""baz"": ""boo"",
+              ""foo"": ""bar""
+            }
+            ");
+
+            var unpacked = UnpackedJsonElement.Unpack(doc.RootElement);
+            var unpackedValue = UnpackedJsonElement.Unpack(value.RootElement);
+
+            var location = JsonPointer.Parse(@"/baz");
+
+            Assert.IsTrue(location.TryAdd(ref unpacked, unpackedValue));
+            JsonDocument result = unpacked.ToJsonDocument();
+
+            JsonElementEqualityComparer.Instance.Equals(expected.RootElement,
+                                                        result.RootElement);
+        }
     }
 }
