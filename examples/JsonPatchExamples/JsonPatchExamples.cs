4c290cfc26fb2e470a14143a45b0130bd87cd163
danielaparker <danielaparker@yahoo.com>
2021-07-19 12:30:35 -0400

    Added JsonPatchExamples

--- examples/JsonPatchExamples/JsonPatchExamples.cs
+++ examples/JsonPatchExamples/JsonPatchExamples.cs
@@ -8,24 +8,35 @@ namespace JsonCons.JsonPatchLib.Examples
 {
     class JsonPatchExamples
     {
+        // Source: http://jsonpatch.com/
         static void AddOperation()
         {
             using var doc = JsonDocument.Parse(@"
 {
-    ""baz"": ""qux"",
-    ""foo"": [ ""a"", 2, ""c"" ]
+  ""baz"": ""qux"",
+  ""foo"": ""bar""
 }
             ");
+
             using var patch = JsonDocument.Parse(@"
 [
-   { ""op"": ""test"", ""path"": ""/baz"", ""value"": ""qux"" },
-   { ""op"": ""test"", ""path"": ""/foo/1"", ""value"": 2 }
+  { ""op"": ""replace"", ""path"": ""/baz"", ""value"": ""boo"" },
+  { ""op"": ""add"", ""path"": ""/hello"", ""value"": [""world""] },
+  { ""op"": ""remove"", ""path"": ""/foo"" }
 ]
             ");
 
+            var options = new JsonSerializerOptions();
+            options.WriteIndented = true;
+
             JsonDocument result = JsonPatch.ApplyPatch(doc.RootElement, patch.RootElement);
 
-            Console.WriteLine($"{JsonSerializer.Serialize(result)}\n\n");
+            Console.WriteLine("The original document:\n");
+            Console.WriteLine($"{JsonSerializer.Serialize(doc.RootElement, options)}\n");
+            Console.WriteLine("The patch:\n");
+            Console.WriteLine($"{JsonSerializer.Serialize(patch.RootElement, options)}\n");
+            Console.WriteLine("The result:\n");
+            Console.WriteLine($"{JsonSerializer.Serialize(result, options)}\n");
         }
 
         static void Main(string[] args)
