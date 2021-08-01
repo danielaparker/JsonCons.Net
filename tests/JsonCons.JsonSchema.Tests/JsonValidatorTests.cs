8bf191eb31c0eb7e55aef82c6aa356047317e412
danielaparker <danielaparker@yahoo.com>
2021-08-01 18:52:44 -0400

    Add JsonSchema

---
+++ tests/JsonCons.JsonSchema.Tests/JsonValidatorTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using JsonCons.JsonSchema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonCons.JsonSchema.Tests
+{
+	public class JsonValidatorTests
+	{
+       [TestMethod]
+       public void Test1()
+       {
+       }
+   }
+}
